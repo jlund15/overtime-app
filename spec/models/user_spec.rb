8118325c160002ce15bac5aff6c96f0594b0d856
Jared Lund <jared.lund@icloud.com>
2017-05-11 06:06:15 -0600

    Used the resource generator for posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "creation" do
     before do
-      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
     end
 
     it "can be created" do
@@ -11,8 +11,8 @@ RSpec.describe User, type: :model do
     end
 
     it "cannot be created without first_name, last_name" do
-      @user.first_name =  nil
-      @user.first_name =  nil
+      @user.first_name = nil
+      @user.last_name = nil
       expect(@user).to_not be_valid
     end
   end
