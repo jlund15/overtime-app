956d0ee5353db05c78e43933a323768c8158f594
Jared Lund <jared.lund@icloud.com>
2017-05-17 16:30:39 -0600

    Installed pundant for permission structure

--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -19,7 +19,18 @@ describe 'navigate' do
       expect(@post.reload.status).to eq('approved')
     end
 
-    xit 'cannot be edited by a non admin' do
+    it 'can be edited by an admin' do
+
+    end
+
+    it 'cannot be edited by a non admin' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      visit edit_post_path(@post)
+
+      expect(page).to_not have_content('Approved')
     end
   end
 end
